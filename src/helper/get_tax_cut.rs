3fda9fe674e1f3475f706067db8834a0370b7beb
hamam <hamam.nasrodin@appfuxion.com>
2025-12-03 16:45:53 +0800

    feat(helper): add tax cut calculation based on marital status and children
    Implement function to determine tax cut amount using PTKP data constants

---
+++ src/helper/get_tax_cut.rs
@@ -0,0 +1,16 @@
+#[path = "../constant/data_ptkp.rs"]
+mod data_ptkp;
+use self::data_ptkp::DATA_PTKP;
+
+pub fn get_tax_cut(is_already_married: &bool, number_of_children: &u32) -> u128 {
+    if !(*is_already_married) {
+        return DATA_PTKP.tk;
+    }
+
+    match number_of_children {
+        0 => DATA_PTKP.k0,
+        1 => DATA_PTKP.k1,
+        2 => DATA_PTKP.k2,
+        _ => DATA_PTKP.k3,
+    }
+}
