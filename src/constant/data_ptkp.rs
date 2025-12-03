2f9b7835358e50e39397121590a6f1be669cfd11
hamam <hamam.nasrodin@appfuxion.com>
2025-12-03 16:45:41 +0800

    feat(constants): add PTKP tax exemption data structure
    Introduce PTKP struct and constant values for Indonesian tax calculation

---
+++ src/constant/data_ptkp.rs
@@ -0,0 +1,16 @@
+
+pub struct PTKP {
+    pub tk: u128,
+    pub k0: u128,
+    pub k1: u128,
+    pub k2: u128,
+    pub k3: u128,
+}
+
+pub const DATA_PTKP: PTKP = PTKP {
+    tk: 54000000,
+    k0: 58500000,
+    k1: 63000000,
+    k2: 67500000,
+    k3: 72000000,
+};
