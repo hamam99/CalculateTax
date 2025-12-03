2a2095864c3e179dc296d765f0aeb6cc8cfc5e9c
hamam <hamam.nasrodin@appfuxion.com>
2025-12-03 16:37:35 +0800

    refactor: move tax calculation logic to separate modules

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 use std::io::{self, Write};
+#[path = "constant/data_ptkp.rs"]
+mod data_ptkp;
 
-struct PTKP {
-    tk: u128,
-    k0: u128,
-    k1: u128,
-    k2: u128,
-    k3: u128,
-}
+#[path = "helper/get_tax_rate.rs"]
+mod get_tax_rate;
+use crate::get_tax_rate::get_tax_rate;
 
-const DATA_PTKP: PTKP = PTKP {
-    tk: 54000000,
-    k0: 58500000,
-    k1: 63000000,
-    k2: 67500000,
-    k3: 72000000,
-};
-
-
+#[path = "helper/get_tax_cut.rs"]
+mod get_tax_cut;
+use crate::get_tax_cut::get_tax_cut;
 
 fn main() {
 
@@ -116,30 +108,5 @@ fn input_yes_no(prompt: &str) -> bool {
             "n" | "N" => return false,
             _ => println!("❌ Invalid input. Please try again.\n"),
         }
-    }
-}
-
-fn get_tax_cut(is_already_married: &bool, number_of_children: &u32) -> u128 {
-    if !(*is_already_married) {
-        return DATA_PTKP.tk;
-    }
-
-    match number_of_children {
-        0 => DATA_PTKP.k0,
-        1 => DATA_PTKP.k1,
-        2 => DATA_PTKP.k2,
-        _ => DATA_PTKP.k3,
-    }
-}
-
-
-
-fn get_tax_rate(salary_taxed: u128) -> u32 {
-    match salary_taxed {
-        0..=60_000_000 => 5,
-        60_000_001..=250_000_000 => 15,
-        250_000_001..=500_000_000 => 25,
-        500_000_001..=5_000_000_000 => 30,
-        _ => 35,
     }
 }
